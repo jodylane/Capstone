20d917d3c94f787a7e844f6e49cd52b1b8e94b6a
Josh Lane <joshuadlane85@gmail.com>
2018-03-30 19:17:20 +0000

    book and user work

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ActiveRecord::Base
+  
+  validates :title, presence: true,
+            uniqueness: { case_sensitive: false }
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :isbn, presence: true
+  validates :publish_date, presence: true
+  validates :publisher, presence: true
+  validates :description, presence: true,
+            length: { minimum: 20, maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable
+    
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false},
+            format: { with: VALID_EMAIL_REGEX }
+  validates :f_name, presence: true
+  validates :l_name, presence: true
+  validates :dob, presence: true
 end
