7af2135682f08c9beaae35872350ebb4f8c06e58
Josh Lane <joshuadlane85@gmail.com>
2018-04-06 18:42:36 +0000

    code cleanup removal of comments

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,6 @@ class BooksController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :home]
   before_action :require_admin, except: [:index, :show, :home]
   
-  # GET /books
-  # GET /books.json
   def index
     @books = Book.all
   end
@@ -12,23 +10,19 @@ class BooksController < ApplicationController
   def home
     redirect_to books_path if user_signed_in?
   end
-
-  # GET /books/1
-  # GET /books/1.json
+  
   def show
+
   end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
-
-  # GET /books/1/edit
+  
   def edit
+
   end
 
-  # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
 
@@ -43,8 +37,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
     respond_to do |format|
       if @book.update(book_params)
@@ -57,8 +49,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
     respond_to do |format|
@@ -68,12 +58,10 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
     def book_params
       params.require(:book).permit(:title, :author, :genre, :isbn, :publish_date, :publisher, :description, :image)
     end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   
   private
-  # this method overrides Devise RegistrationsController method to accept our new params that we have added to signup
   def sign_up_params
     params.require(:user).permit(:f_name, :l_name, :email, :dob, :phone, :password, :password_confirmation)
   end
